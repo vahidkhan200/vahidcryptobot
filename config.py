ad2e61ea966dd1c362d4912e7d5b92000f582d4e
vahidkhan200 <vahidelder200@gmail.com>
2025-04-16 13:35:22 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# config.py
+
+# توکن ربات تلگرام
+TELEGRAM_TOKEN = "7808088088:AAGu9D1Vr5Iq6lrrE7P2jbMr32_-K6Y8wF4"
+
+# آی‌دی چت برای ارسال پیام
+TELEGRAM_CHAT_ID = "392018191"
+
+# آدرس API صرافی LBank
+LBANK_API_URL = "https://api.lbank.info/v1/kline.do"
+
+# لیست ارزهایی که بررسی می‌کنی
+SYMBOLS = ['btc_usdt', 'eth_usdt', 'bnb_usdt']
